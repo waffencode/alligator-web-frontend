1fe0f2beefce1117f51c57451888f22a8ef17e08
OlgaRhythm <y.o.a@yandex.ru>
2024-05-18 19:29:47 +0300

    Register form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import AuthPage from '../pages/auth/AuthPage';
+import RegisterPage from '../pages/auth/RegisterPage';
 
 const App: React.FC = () => {
   return (
     <div>
-      <AuthPage />
+      <RegisterPage />
     </div>
   );
 }
---
+++ src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import './AuthPage.css';
+
+const RegisterPage: React.FC = () => {
+    const [name, setName] = React.useState('');
+    const [surname, setSurname] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [message, setMessage] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [repPassword, setRepPassword] = React.useState('');
+
+    const emailErrorMessageText: string = "Неправильный email";
+    const errorMessageTextNotEqual: string = "Пароли не совпадают";
+
+
+    const handleRegisterSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        setMessage(''); // Сброс сообщения об ошибке перед отправкой
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(username)) {
+            setMessage(emailErrorMessageText);
+        } else if (password !== repPassword) {
+            setMessage(errorMessageTextNotEqual);
+        } else {
+            try {
+                const response = await fetch('http://localhost:8080/register', { //194.87.234.28
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ username, password })
+                });
+    
+                if (!response.ok) {
+                    setMessage('Ошибка при регистрации. Попробуйте еще раз.');
+                } else {
+                    const token = await response.text();
+                    // Сохранение токена и редирект на защищенную страницу
+                    localStorage.setItem('token', token);
+                    window.location.href = '/dashboard';
+                }
+            } catch (error) {
+                setMessage('Ошибка при аутентификации. Попробуйте еще раз.');
+            }
+        }        
+    };
+
+    return (
+        <div className="container">
+            <div className="form-container">
+                <h2>Регистрация</h2>
+                <label className="error-message">{message}</label>
+                <form onSubmit={handleRegisterSubmit}>
+                    <div className="input-group">
+                        <label>Имя</label><br />
+                        <input
+                            type="text"
+                            id="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label>Фамилия</label><br />
+                        <input
+                            type="text"
+                            id="surname"
+                            value={surname}
+                            onChange={(e) => setSurname(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="username">Email</label><br />
+                        <input
+                            type="text"
+                            id="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Пароль</label><br />
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Повторите пароль</label><br />
+                        <input
+                            type="password"
+                            id="rep-password"
+                            value={repPassword}
+                            onChange={(e) => setRepPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="submit-button">Регистрация</button><br />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;
