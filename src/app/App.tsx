ba735f9308d11c907e7eea97c6e89aeab153c679
OlgaRhythm <y.o.a@yandex.ru>
2024-05-18 16:33:56 +0300

    Auth form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../shared/assets/logo.svg';
 import AuthPage from '../pages/auth/AuthPage';
 
 const App: React.FC = () => {
--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './AuthPage.css';
 
 const AuthPage: React.FC = () => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [message, setMessage] = React.useState('');
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -10,31 +12,36 @@ const AuthPage: React.FC = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+    <div className="container">
+      <div className="form-container">
+        <h2>Войти</h2>
+        <label className="error-message">{message}</label>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Эл. почта</label><br />
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Пароль</label><br />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <a className="link" href="#">Забыли пароль?</a><br />
+          <button type="submit" className="submit-button">Вход</button><br />
+          <a className="link" href="#">Нет учётной записи? Зарегистрироваться...</a><br />
+        </form>
+      </div>
     </div>
   );
 };
