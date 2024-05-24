defca497f8b839ae33f731e976735138e7c1cae3
OlgaRhythm <y.o.a@yandex.ru>
2024-05-24 19:38:44 +0300

    Page with list of user's teams

--- src/pages/teams/AvailableTeamsPage.tsx
+++ src/pages/teams/AvailableTeamsPage.tsx
@@ -9,10 +9,12 @@ import sprintIcon from '../../shared/ui/icons/sprint.png';
 import { getTeamsByUserIdWithCountOfMembers } from '../../shared/api';
 import { SliderItemsGenerator } from '../../widgets/SliderItemsGenerator';
 import { Team } from '../../shared/api/IResponses';
+import { useNavigate } from 'react-router-dom';
 
 const AvaliableTeamsPage: React.FC = () => {
        
     const menuItems = SliderItemsGenerator(); // Получаем элементы меню
+    const navigate = useNavigate(); 
 
     const [teams, setTeams] = useState<Team[]>([]);
     const [error, setError] = useState<string | null>(null);
@@ -33,15 +35,29 @@ const AvaliableTeamsPage: React.FC = () => {
             setError('No authentication token found');
         }
     }, []);
+/*
+    const teams: Team[] = [
+        { id: '1', name: 'Codebreakers', participants: 5 },
+        { id: '2', name: 'Команда2', participants: 8 },
+        { id: '3', name: 'Команда3', participants: 4 },
+    ];*/
 
     return (
-        <div className="profile-page">
+        <div className="avaliable-teams-page">
             <Sidebar menuItems={menuItems} headerIcon={alligatorIcon} />
-            <div className="profile-content">
+            <div className="teams-content">
                 <h1>Доступные команды</h1>
                 {error ? (<div className="error-message">{error}</div>) : ('')}
-                <div className="profile-info">
-                        
+                <div className="teams-list">
+                    {teams.map((team) => (
+                        <div key={team.id} className="team-tile">
+                            <div className="team-info">
+                                <h2>{team.name}</h2>
+                                <p>{team.memberCount} участников</p>
+                            </div>
+                            <button className="navigate-button" onClick={() => navigate('/teams/${team.id}')}>Перейти</button>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
