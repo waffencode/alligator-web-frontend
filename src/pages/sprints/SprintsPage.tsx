033c574ab43862f93ef57901340c29e6e50e50e5
OlgaRhythm <y.o.a@yandex.ru>
2024-05-25 01:48:26 +0300

    Sprint page gets info from server

--- src/pages/sprints/SprintsPage.tsx
+++ src/pages/sprints/SprintsPage.tsx
@@ -3,26 +3,37 @@ import Sidebar from '../../widgets/Sidebar';
 import './SprintsPage.css';
 import alligatorIcon from '../../shared/ui/icons/alligator.png';
 import { SliderItemsGenerator } from '../../widgets/SliderItemsGenerator';
+import { getSprintsByUserId } from '../../shared/api';
+import { Sprint } from '../../shared/api/IResponses';
+import { format } from 'date-fns';
 
 const SprintsPage: React.FC = () => {
     const menuItems = SliderItemsGenerator(); // Получаем элементы меню
+    const [sprints, setSprints] = useState<Sprint[]>([]);;
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (token) {
-            
+            getSprintsByUserId(token).
+            then((sprints) => {
+                setSprints(sprints);
+            })
+            .catch((err) => {
+                console.error('Failed to fetch user profile', err);
+                setError('Failed to load user profile');
+            });
         } else {
             setError('No authentication token found');
         }
     }, []);
-
+/*
     const sprints = [
         { name: 'Sprint 1', team: 'Sprint 1', scrumMaster: 'Scrum', startTime: '06-11-2024', endTime: '07-01-2024', sp: '12', state: 'ACTIVE', onClick: () => {} },
         { name: 'Sprint 2', team: 'Sprint 2', scrumMaster: 'Master', startTime: '66.44.44', endTime: '58.66.44', sp: '15', state: 'ACTIVE', onClick: () => {} },
         { name: 'Sprint 3', team: 'Sprint 3', scrumMaster: 'Scam', startTime: '456.454.4', endTime: '456.13.2', sp: '43', state: 'ACTIVE', onClick: () => {} },
       ];
-
+*/
     return (
         <div className="profile-page">
             <Sidebar menuItems={menuItems} headerIcon={alligatorIcon} />
@@ -42,12 +53,12 @@ const SprintsPage: React.FC = () => {
                         <div>Статус</div>
                     </div>
                     {sprints.map((sprint, index) => (
-                        <div key={index} className="sprint-tile" onClick={sprint.onClick}>
+                        <div key={index} className="sprint-tile"onClick={() => {}}>
                             <div>{sprint.name}</div>
-                            <div>{sprint.team}</div>
-                            <div>{sprint.scrumMaster}</div>
-                            <div>{sprint.startTime}</div>
-                            <div>{sprint.endTime}</div>
+                            <div>{sprint.team.name}</div>
+                            <div>{sprint.scrumMaster.user.username}</div>
+                            <div>{format(new Date(sprint.startTime), 'dd.MM.yyyy')}</div>
+                            <div>{format(new Date(sprint.endTime), 'dd.MM.yyyy')}</div>
                             <div>{sprint.sp}</div>
                             <div>{sprint.state}</div>
                         </div>
