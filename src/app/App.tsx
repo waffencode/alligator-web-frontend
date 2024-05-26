579ef092a96ee911c6671d6d6277b87da986466b
dictator_zx <ivanandrosovv@gmail.com>
2024-05-26 20:54:58 +0300

    Imports cleanup

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import AuthPage from '../pages/auth/AuthPage';
-import RegisterPage from '../pages/auth/RegisterPage';
-import Profile from '../pages/profile/ProfilePage';
-
-import AppRoutes from '../pages/AppRoutes';
 import AppRouter from "./AppRouter";
 import {BrowserRouter} from "react-router-dom";
 import ApiContextProvider from "../widgets/ApiContextProvider/ApiContextProvider";
